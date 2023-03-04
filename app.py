bfdaab543c39dffc7734fb08b66a07f910fc419a
Henry Roberts <hbjroberts@gmail.com>
2023-03-04 22:09:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,8 @@ app.layout = \
 dbc.Container\
 ([
     html.Br(),
+    dbc.Row(dbc.Card(dbc.CardBody(html.H5('Build a British Voter! Enter demographic characteristics to see the probability of party support at the 2019 election')), style={'width':'100%'})),
+    html.Br(),
     dbc.Row(dbc.Stack([
         dbc.Card(dbc.CardBody('A')),
         dbc.Card(dbc.CardBody(dcc.Dropdown({'White British': 'White British,', 'Any other white background': 'other White,', 
